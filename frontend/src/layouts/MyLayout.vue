bafdab2dc7ce3c59a12fa8c6a17d631caf460d5a
celinehuang <celine756@hotmail.com>
2019-11-16 17:15:27 -0500

    displaying items found in uploaded file

--- frontend/src/layouts/MyLayout.vue
+++ frontend/src/layouts/MyLayout.vue
@@ -13,26 +13,44 @@
       <q-page class="flex column flex-center">
         <input
           type="file"
+          style="margin-top: 50px"
           @change="onFileChanged"
         >
         <div>
           <p> </p>
         </div>
         <q-btn
-              color="secondary"
-              class="glossy"
-              @click="sendFile"
-              label="UPLOAD"
+          Updated
+          upstream
+          color="secondary"
+          class="glossy"
+          @click="sendFile"
+          label="UPLOAD"
         />
         <div>
           <p> </p>
         </div>
         <div v-if="isLoading==true">
-            <component
+          <component
             :is="`q-spinner-hourglass`"
             color='secondary'
-            size= '36px'
+            size='36px'
           />
+        </div>
+        <q-btn
+          color="secondary"
+          class="glossy"
+          @click="sendFile"
+          label="Evaluate"
+        />
+        <div v-if="resultFetched==true">
+          <DetectedObject
+            v-for="result in response_result"
+            v-bind:key="result.id"
+            :link='result.link'
+            :object='result.metadata.name'
+            v-bind:percentageCertainty='parseInt(result.metadata.percentage_probability)'
+          ></DetectedObject>
         </div>
       </q-page>
     </q-page-container>
@@ -40,12 +58,18 @@
 </template>
 
 <script>
+import DetectedObject from '../components/DetectedObject.vue'
 export default {
   name: 'MyLayout',
+  components: {
+    DetectedObject
+  },
   data () {
     return {
       file: null,
-      isLoading: false
+      isLoading: false,
+      resultFetched: false,
+      response_result: []
     }
   },
   methods: {
@@ -59,6 +83,11 @@ export default {
       this.$axios.post('http://localhost:5000/upload', formData, { headers: { 'Content-Type': 'multipart/form-data' } })
         .then(resp => {
           this.isLoading = false
+          this.resultFetched = true
+          this.response_result = resp.data
+          for (let i = 1; i <= resp.data.length; i++) {
+            this.response_result[i]['id'] = i
+          }
         })
         .catch(error => {
           console.log(error.resp)
