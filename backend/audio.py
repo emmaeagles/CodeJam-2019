95cea58f1f53e0fee8366a96e297a0d9986fe1b8
PaulHooley <paul.hooley@mail.mcgill.ca>
2019-11-15 22:42:18 -0500

    mf uhhh

--- backend/audio.py
+++ backend/audio.py
@@ -1,7 +1,7 @@
 from pydub import AudioSegment
 
-sound1 = AudioSegment.from_file("./Bodak-Yellow.wav")
-sound2 = AudioSegment.from_file("./September.wav")
+sound1 = AudioSegment.from_file("./audioFiles/Bodak-Yellow.wav")
+sound2 = AudioSegment.from_file("./audioFiles/September.wav")
 
 combined = sound1.overlay(sound2)
 
